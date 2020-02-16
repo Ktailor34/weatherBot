98edb8c2afa29b107d36c0aa55615f7b76b9da27
Kishan Tailor <ktailor@Kishans-MacBook-Air.local>
2020-02-16 00:27:50 -0500

    refactoring

--- weatherBot.py
+++ weatherBot.py
@@ -7,6 +7,11 @@ import json
 url = "https://community-open-weather-map.p.rapidapi.com/weather"
 querystring = {"lat":"35.9606","lon":"83.9207","units":"%22imperial%22","q":"knoxville"}
 
+headers = {
+    'x-rapidapi-host': hiddenWeather,
+    'x-rapidapi-key': hiddenWeatherSecret
+    }
+
 responseJson = requests.request("GET", url, headers=headers, params=querystring)
 response = json.loads(responseJson.text)
 
@@ -15,12 +20,12 @@ response["main"] = "Drizzle"
 if (response["main"] in ["Drizzle", "Rain", "Snow", "Thunderstorm"]):
 
 	# the following line needs your Twilio Account SID and Auth Token
-	client = Client(config[0], config[1])
+	client = Client(hiddenText, hiddenTextSecret)
 
 	# change the "from_" number to your Twilio number and the "to" number
 	# to the phone number you signed up for Twilio with, or upgrade your
 	# account to send SMS to any phone number
-	client.messages.create(to="+18657732626", from_="+19177461747", body=response["main"])
+	client.messages.create(to=phoneTo, from_=phoneFrom, body=response["main"])
 	print(response["description"])
 
 
