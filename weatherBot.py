89dca722d930eec5a8c194faf794c1aa84c99af0
Kishan Tailor <ktailor@Kishans-MacBook-Air.local>
2020-02-15 21:58:49 -0500

    security fix

--- weatherBot.py
+++ weatherBot.py
@@ -8,4 +8,4 @@ client = Client(config[0], config[1])
 # change the "from_" number to your Twilio number and the "to" number
 # to the phone number you signed up for Twilio with, or upgrade your
 # account to send SMS to any phone number
-client.messages.create(to="+18656845102", from_="+19177461747", body="Hi Dad")
+client.messages.create(to="+18657732626", from_="+19177461747", body="Hi Dad")
