d37c86cc3c08a55c8d83761cc7c472725f98c113
Kishan Tailor <ktailor@Kishans-MacBook-Air.local>
2020-02-15 22:53:44 -0500

    added weather api

--- weatherBot.py
+++ weatherBot.py
@@ -1,11 +1,25 @@
 # we import the Twilio client from the dependency we just installed
 from twilio.rest import Client
-from config import config
+from config import *
+import requests
+import json
 
-# the following line needs your Twilio Account SID and Auth Token
-client = Client(config[0], config[1])
+responseJson = requests.request("GET", url, headers=headers, params=querystring)
+response = json.loads(responseJson.text)
 
-# change the "from_" number to your Twilio number and the "to" number
-# to the phone number you signed up for Twilio with, or upgrade your
-# account to send SMS to any phone number
-client.messages.create(to="+18657732626", from_="+19177461747", body="Hi Dad")
+print(response)
+
+if (response["main"] in ["Drizzle", "Rain", "Snow", "Thunderstorm"]):
+
+	# the following line needs your Twilio Account SID and Auth Token
+	client = Client(config[0], config[1])
+
+	# change the "from_" number to your Twilio number and the "to" number
+	# to the phone number you signed up for Twilio with, or upgrade your
+	# account to send SMS to any phone number
+	client.messages.create(to="+18657732626", from_="+19177461747", body=response["main"].text)
+	print(response.text)
+
+
+
+
