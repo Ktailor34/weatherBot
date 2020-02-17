2fe5d46781fd0d9d62e62cfac47217467b2579e0
Kishan Tailor <ktailor@Kishans-MacBook-Air.local>
2020-02-16 19:33:04 -0500

    env variables

--- weatherBot.py
+++ weatherBot.py
@@ -9,8 +9,8 @@ url = "https://community-open-weather-map.p.rapidapi.com/weather"
 querystring = {"lat":"35.9606","lon":"83.9207","units":"%22imperial%22","q":"knoxville"}
 
 headers = {
-    'x-rapidapi-host': os.environ.get(hiddenWeather),
-    'x-rapidapi-key': os.environ.get(hiddenWeatherSecret)
+    'x-rapidapi-host': os.environ.get("hiddenWeather"),
+    'x-rapidapi-key': os.environ.get("hiddenWeatherSecret")
     }
 
 responseJson = requests.request("GET", url, headers=headers, params=querystring)
@@ -21,7 +21,7 @@ response["main"] = "Drizzle"
 if (response["main"] in ["Drizzle", "Rain", "Snow", "Thunderstorm"]):
 
 	# the following line needs your Twilio Account SID and Auth Token
-	client = Client(os.environ.get(hiddenText), os.environ.get(hiddenTextSecret))
+	client = Client(os.environ.get("hiddenText"), os.environ.get("hiddenTextSecret"))
 
 	# change the "from_" number to your Twilio number and the "to" number
 	# to the phone number you signed up for Twilio with, or upgrade your
