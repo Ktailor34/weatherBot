5f38d2de4f98dcc04b321308efa28ca27b78329d
Kishan Tailor <Tailorkishan123@gmail.com>
2020-02-17 10:27:54 -0500

    Update weatherBot.py

--- weatherBot.py
+++ weatherBot.py
@@ -26,7 +26,7 @@ if (response["main"] in ["Drizzle", "Rain", "Snow", "Thunderstorm"]):
 	# change the "from_" number to your Twilio number and the "to" number
 	# to the phone number you signed up for Twilio with, or upgrade your
 	# account to send SMS to any phone number
-	client.messages.create(to=os.environ.phoneTo, from_=os.environ.phoneFrom, body=response["main"])
+	client.messages.create(to=os.environ.get("phoneTo"), from_=os.environ.get("phoneFrom"), body=response["main"])
 	print(response)
 	print(response["description"])
 
